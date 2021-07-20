98b6f0d2ef4c80ffb627b8e67954aaa966dd4a42
gstimm <contato.gabrieltimm@gmail.com>
2021-07-19 21:15:28 -0300

    feat: register, session, password reset and email

---
+++ start/routes.js
@@ -0,0 +1,10 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+
+Route.post('users', 'UserController.store')
+
+Route.post('sessions', 'SessionController.store')
+
+Route.post('forgot-password', 'ForgotPasswordController.store')
