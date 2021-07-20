351a608ac960f0de101506188948d5579bd3d5ee
gstimm <contato.gabrieltimm@gmail.com>
2021-07-20 13:07:44 -0300

    feat: add routes validation

--- start/routes.js
+++ start/routes.js
@@ -1,20 +1,23 @@
 'use strict'
 
-/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
 Route.post('users', 'UserController.store').validator('User')
-Route.post('sessions', 'SessionController.store')
+Route.post('sessions', 'SessionController.store').validator('Session')
 
-Route.post('forgot-password', 'ForgotPasswordController.store')
-Route.put('forgot-password', 'ForgotPasswordController.update')
+Route.post('passwords', 'ForgotPasswordController.store').validator('ForgotPassword')
+Route.put('passwords', 'ForgotPasswordController.update').validator('ResetPassword')
 
 Route.get('/files/:id', 'FileController.show')
 
 Route.group(() => {
   Route.post('/files', 'FileController.store')
 
-  Route.resource('projects', 'ProjectController').apiOnly()
+  Route.resource('projects', 'ProjectController')
+    .apiOnly()
+    .validator(new Map([[['projects.store'], ['Project']]]))
 
-  Route.resource('projects.tasks', 'TaskController').apiOnly()
+  Route.resource('projects.tasks', 'TaskController')
+    .apiOnly()
+    .validator(new Map([[['projects.tasks.store'], ['Task']]]))
 }).middleware(['auth'])
