ec0134fa061e28a5d93108e79e777f66b15d73d8
gstimm <contato.gabrieltimm@gmail.com>
2021-07-20 11:26:50 -0300

    feat: add file upload

--- start/routes.js
+++ start/routes.js
@@ -4,9 +4,9 @@
 const Route = use('Route')
 
 Route.post('users', 'UserController.store')
-
 Route.post('sessions', 'SessionController.store')
 
 Route.post('forgot-password', 'ForgotPasswordController.store')
-
 Route.put('forgot-password', 'ForgotPasswordController.update')
+
+Route.post('/files', 'FileController.store')
